d14b01474edf4581129e7393683302a1d3c987b4
ayazadeh <50027@hfz.com>
2024-09-28 19:21:03 +0330

    TS: project 2 -> Type Aliases _ Custom Types

--- 2/app.ts
+++ 2/app.ts
@@ -1,6 +1,10 @@
 // union types, type1 | type2
 // literal types, 'as-number' | 'as-text'
-function combine(input1: number | string, input2: number | string, resultType: 'as-number' | 'as-text') {
+
+type Combinable = number | string;
+type ResultTypeDescriptor = "as-number" | "as-text";
+
+function combine(input1: Combinable, input2: Combinable, resultType: ResultTypeDescriptor) {
 	let result;
 	if (typeof input1 === "number" && typeof input2 === "number") {
 		result = input1 + input2;
