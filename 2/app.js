02d171f1d20bbff961333152fc75999ed512a146
ayazadeh <50027@hfz.com>
2024-09-28 19:08:45 +0330

    TS: project 2 -> Literal Types

--- 2/app.js
+++ 2/app.js
@@ -1,14 +1,23 @@
-function combine(input1, input2) {
+// union types, type1 | type2
+// literal types, 'as-number' | 'as-text'
+function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
+    if (typeof input1 === "number" && typeof input2 === "number") {
         result = input1 + input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultType === "as-number") {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var combineAges = combine(30, 26);
+var combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
-var combineNames = combine('Mohammad', ' Ayazadeh');
+var combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+var combineNames = combine("Mohammad", " Ayazadeh", "as-text");
 console.log(combineNames);
