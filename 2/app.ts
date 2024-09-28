5f35263c52ad3cb0c00f268b873e2bf042ee87e6
ayazadeh <50027@hfz.com>
2024-09-28 18:43:23 +0330

    TS: project 2 -> Working with Enums

--- 2/app.ts
+++ 2/app.ts
@@ -1,20 +1,34 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[],
-  role: [number, string] // tuple
-} = {
-  name: "Mohammad",
-  age: 25,
-  hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'] // tuple
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[],
+//   role: [number, string] // tuple
+// } = {
+//   name: "Mohammad",
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'] // tuple
+// };
 
 // person.role[1] = 20; // error
 // person.role = ['r', 2]; // error
 
-console.log(person.name);
+// console.log(person.name);
 
-for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// }
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+console.log('Role', Role.ADMIN, Role.READ_ONLY, Role.AUTHOR);
+
+const person = {
+	name: "Mohammad",
+	age: 25,
+	hobbies: ["Sports", "Cooking"],
+	role: Role.ADMIN 
+};
+
+if (person.role === Role.ADMIN) {
+	console.log('is admin');
 }
