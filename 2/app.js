bc0b1d8844034f071fd77c3f279cf4393da0ec3b
ayazadeh <50027@hfz.com>
2024-09-28 19:43:28 +0330

    TS: project 2 -> Functions as Types

--- 2/app.js
+++ 2/app.js
@@ -1,7 +1,14 @@
 function add(n1, n2) {
+    // we can don't specify the return type
     return n1 + n2;
 }
 function printResult(num) {
+    // if we don't return anything we can specify void
     console.log("Result: " + num);
 }
 printResult(add(5, 12));
+// function type
+var combineValues;
+combineValues = add;
+// combineValues = printResult; // error
+console.log(combineValues(8, 8));
--- 2/app.ts
+++ 2/app.ts
@@ -1,9 +1,19 @@
-function add(n1: number, n2: number): number { // we can don't specify the return type
+function add(n1: number, n2: number): number {
+	// we can don't specify the return type
 	return n1 + n2;
 }
 
-function printResult(num: number): void { // if we don't return anything we can specify void
+function printResult(num: number): void {
+	// if we don't return anything we can specify void
 	console.log("Result: " + num);
 }
 
 printResult(add(5, 12));
+
+// function type
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult; // error
+
+console.log(combineValues(8, 8));
