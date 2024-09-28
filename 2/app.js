7de473813f784e47563965951521a00cd65ee81b
ayazadeh <m.ayazadeh@gmail.com>
2024-09-28 18:43:23 +0330

    TS: project 2 -> Working with Enums

--- 2/app.js
+++ 2/app.js
@@ -1,10 +1,34 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[],
+//   role: [number, string] // tuple
+// } = {
+//   name: "Mohammad",
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'] // tuple
+// };
+// person.role[1] = 20; // error
+// person.role = ['r', 2]; // error
+// console.log(person.name);
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+console.log('Role', Role.ADMIN, Role.READ_ONLY, Role.AUTHOR);
 var person = {
     name: "Mohammad",
     age: 25,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
