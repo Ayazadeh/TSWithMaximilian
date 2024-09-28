c044e44b86a1b85a7efe5f71b5c25ea323aeb60b
ayazadeh <50027@hfz.com>
2024-09-28 18:58:03 +0330

    TS: project 2 -> Union Types

--- 2/app.js
+++ 2/app.js
@@ -1,34 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[],
-//   role: [number, string] // tuple
-// } = {
-//   name: "Mohammad",
-//   age: 25,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author'] // tuple
-// };
-// person.role[1] = 20; // error
-// person.role = ['r', 2]; // error
-// console.log(person.name);
-// for (const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase());
-// }
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-console.log('Role', Role.ADMIN, Role.READ_ONLY, Role.AUTHOR);
-var person = {
-    name: "Mohammad",
-    age: 25,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN
-};
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine('Mohammad', ' Ayazadeh');
+console.log(combineNames);
---
+++ 2/obj-arrays-enum.ts
@@ -0,0 +1,34 @@
+// const person: { // object
+//   name: string;
+//   age: number;
+//   hobbies: string[], // array
+//   role: [number, string] // tuple
+// } = {
+//   name: "Mohammad",
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'] // tuple
+// };
+
+// person.role[1] = 20; // error
+// person.role = ['r', 2]; // error
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// }
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+console.log('Role', Role.ADMIN, Role.READ_ONLY, Role.AUTHOR);
+
+const person = {
+	name: "Mohammad",
+	age: 25,
+	hobbies: ["Sports", "Cooking"],
+	role: Role.ADMIN 
+};
+
+if (person.role === Role.ADMIN) {
+	console.log('is admin');
+}
