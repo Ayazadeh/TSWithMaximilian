aa1c2907880d087a259f0a24b2f8ec4720c22001
ayazadeh <m.ayazadeh@gmail.com>
2024-09-28 19:08:45 +0330

    TS: project 2 -> Literal Types

--- 2/app.ts
+++ 2/app.ts
@@ -1,15 +1,24 @@
-function combine(input1: number | string, input2: number | string) { // union types
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-	return result;
-} 
+// union types, type1 | type2
+// literal types, 'as-number' | 'as-text'
+function combine(input1: number | string, input2: number | string, resultType: 'as-number' | 'as-text') {
+	let result;
+	if (typeof input1 === "number" && typeof input2 === "number") {
+		result = input1 + input2;
+	} else {
+		result = input1.toString() + input2.toString();
+	}
+	if (resultType === "as-number") {
+		return +result;
+	} else {
+		return result.toString();
+	}
+}
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineNames = combine('Mohammad', ' Ayazadeh'); 
+const combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+
+const combineNames = combine("Mohammad", " Ayazadeh", "as-text");
 console.log(combineNames);
