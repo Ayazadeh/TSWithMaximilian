3c1ed4ccc35c0c02f1abf746558c479ef316b913
ayazadeh <50027@hfz.com>
2024-09-28 19:50:43 +0330

    TS: project 2 -> Function Types & Callbacks

--- 2/app.ts
+++ 2/app.ts
@@ -17,3 +17,13 @@ combineValues = add;
 // combineValues = printResult; // error
 
 console.log(combineValues(8, 8));
+
+// callback -> cb
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+	const result = n1 + n2;
+	cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+	console.log(result);
+});
