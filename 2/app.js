9abe8931fcedee739618e9de9d8be551897a55e9
ayazadeh <m.ayazadeh@gmail.com>
2024-09-30 21:54:34 +0330

    TS: project 2 -> The _never_ Type

--- 2/app.js
+++ 2/app.js
@@ -1,21 +1,12 @@
-function add(n1, n2) {
-    // we can don't specify the return type
-    return n1 + n2;
+var userInput; // a type you shouldn't use all the time but it is better than any
+var userName;
+userInput = 5;
+userInput = "test";
+// userName = userInput; // error
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    // if we don't return anything we can specify void
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-printResult(add(5, 12));
-// function type
-var combineValues;
-combineValues = add;
-// combineValues = printResult; // error
-console.log(combineValues(8, 8));
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError("An error occurred!", 500);
