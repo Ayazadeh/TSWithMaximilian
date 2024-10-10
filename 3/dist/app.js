c56f448584f3549aa8543820926a79953661daa4
ayazadeh <m.ayazadeh@gmail.com>
2024-10-10 10:39:25 +0330

    TS: project 3 -> Arrow Functions

--- 3/dist/app.js
+++ 3/dist/app.js
@@ -3,3 +3,18 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     console.log('You clicked me!');
 });
+button.addEventListener('click', event => console.log('arrow Func ', event));
+function add(n1, n2) {
+    return n1 + n2;
+}
+const arrowAdd = (n1, n2) => {
+    return n1 + n2;
+};
+const arrowAdd2 = (n1, n2) => n1 + n2;
+console.log('add ', add(2, 6));
+console.log('arrowAdd ', arrowAdd(2, 6));
+console.log('arrowAdd2 ', arrowAdd2(2, 6));
+const printOutput = (output) => console.log(output);
+const printOutput2 = output => console.log(output);
+printOutput('printOutput ' + add(2, 6));
+printOutput2('printOutput2 ' + add(2, 6));
--- 3/src/app.ts
+++ 3/src/app.ts
@@ -3,3 +3,26 @@ const button = document.querySelector('button')!;
 button.addEventListener('click', () => {
     console.log('You clicked me!');
 });
+
+button.addEventListener('click', event => console.log('arrow Func ', event));
+
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const arrowAdd = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
+const arrowAdd2 = (n1: number, n2: number) => n1 + n2;
+
+console.log('add ', add(2, 6));
+console.log('arrowAdd ', arrowAdd(2, 6));
+console.log('arrowAdd2 ', arrowAdd2(2, 6));
+
+const printOutput = (output: string | number) => console.log(output);
+
+const printOutput2: (a: number | string ) => void = output => console.log(output);
+
+printOutput('printOutput ' + add(2, 6));
+printOutput2('printOutput2 ' + add(2, 6));
