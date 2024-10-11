5955e1187214dfbf94e48a49abd764ebb82b642e
ayazadeh <m.ayazadeh@gmail.com>
2024-10-11 22:30:37 +0330

    TS: project 3 -> Rest Parameters

--- 3/dist/app.js
+++ 3/dist/app.js
@@ -1,9 +1,9 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('You clicked me!');
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("You clicked me!");
 });
-button.addEventListener('click', event => console.log('arrow Func ', event));
+button.addEventListener("click", (event) => console.log("arrow Func ", event));
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -11,21 +11,30 @@ const arrowAdd = (n1, n2) => {
     return n1 + n2;
 };
 const arrowAdd2 = (n1, n2 = 10) => n1 + n2;
-console.log('add ', add(2, 6));
-console.log('arrowAdd ', arrowAdd(2, 6));
-console.log('arrowAdd2 ', arrowAdd2(2, 6));
+console.log("add ", add(2, 6));
+console.log("arrowAdd ", arrowAdd(2, 6));
+console.log("arrowAdd2 ", arrowAdd2(2, 6));
 const printOutput = (output) => console.log(output);
-const printOutput2 = output => console.log(output);
-printOutput('printOutput ' + add(2, 6));
-printOutput2('printOutput2 ' + add(2, 6));
-printOutput('default parameters ' + arrowAdd2(2));
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Skiing'];
+const printOutput2 = (output) => console.log(output);
+printOutput("printOutput " + add(2, 6));
+printOutput2("printOutput2 " + add(2, 6));
+printOutput("default parameters " + arrowAdd2(2));
+// spread operator
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Skiing"];
 activeHobbies.push(...hobbies);
-console.log('spread operator ', activeHobbies);
+console.log("spread operator ", activeHobbies);
 const person = {
-    name: 'Mohammad',
-    age: 26
+    name: "Mohammad",
+    age: 26,
 };
 const copiedPerson = Object.assign({}, person);
-console.log('spread operator ', copiedPerson);
+console.log("spread operator ", copiedPerson);
+// rest parameters
+const sum = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = sum(2, 5, 10, 20, 100, -30);
+console.log('rest parameters: ', addedNumbers);
--- 3/src/app.ts
+++ 3/src/app.ts
@@ -1,43 +1,55 @@
-const button = document.querySelector('button')!;
+const button = document.querySelector("button")!;
 
-button.addEventListener('click', () => {
-    console.log('You clicked me!');
+button.addEventListener("click", () => {
+  console.log("You clicked me!");
 });
 
-button.addEventListener('click', event => console.log('arrow Func ', event));
+button.addEventListener("click", (event) => console.log("arrow Func ", event));
 
 function add(n1: number, n2: number) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
 const arrowAdd = (n1: number, n2: number) => {
-    return n1 + n2;
-}
+  return n1 + n2;
+};
 
 const arrowAdd2 = (n1: number, n2: number = 10) => n1 + n2;
 
-console.log('add ', add(2, 6)); 
-console.log('arrowAdd ', arrowAdd(2, 6));
-console.log('arrowAdd2 ', arrowAdd2(2, 6));
+console.log("add ", add(2, 6));
+console.log("arrowAdd ", arrowAdd(2, 6));
+console.log("arrowAdd2 ", arrowAdd2(2, 6));
 
 const printOutput = (output: string | number) => console.log(output);
 
-const printOutput2: (a: number | string ) => void = output => console.log(output);
+const printOutput2: (a: number | string) => void = (output) =>
+  console.log(output);
 
-printOutput('printOutput ' + add(2, 6));
-printOutput2('printOutput2 ' + add(2, 6));
+printOutput("printOutput " + add(2, 6));
+printOutput2("printOutput2 " + add(2, 6));
 
-printOutput('default parameters ' + arrowAdd2(2));
+printOutput("default parameters " + arrowAdd2(2));
 
-const hobbies = ['Sports', 'Cooking']
-const activeHobbies = ['Skiing']
-activeHobbies.push(...hobbies)
-console.log('spread operator ', activeHobbies);
+// spread operator
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Skiing"];
+activeHobbies.push(...hobbies);
+console.log("spread operator ", activeHobbies);
 
 const person = {
-    name: 'Mohammad',
-    age: 26
-}
+  name: "Mohammad",
+  age: 26,
+};
 
-const copiedPerson = { ...person }
-console.log('spread operator ', copiedPerson);
+const copiedPerson = { ...person };
+console.log("spread operator ", copiedPerson);
+
+// rest parameters
+const sum = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = sum(2, 5, 10, 20, 100, -30)
+console.log('rest parameters: ', addedNumbers);
