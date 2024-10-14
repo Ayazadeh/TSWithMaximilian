6e9f9b728948d3e379931062bf8b09d86c7c75a9
ayazadeh <m.ayazadeh@gmail.com>
2024-10-14 09:56:30 +0330

    TS: project 4 -> _private_ and _public_ Access Modifiers

--- 4/dist/app.js
+++ 4/dist/app.js
@@ -1,14 +1,25 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log('employees length: ', this.employees.length);
+        console.log('employees: ', this.employees);
+    }
 }
 const accounting = new Department('Accounting');
 console.log(accounting);
 accounting.describe();
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-accountingCopy.describe();
+accounting.name = "IT";
+accounting.describe();
+accounting.addEmployee('Mohammad');
+accounting.addEmployee('Max');
+accounting.printEmployeeInformation();
--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    name: string; // public modifier that is default, makes this property accessible from outside the class
+    private employees: string[] = []; // we use private modifier to make sure that we can't access this property from outside the class
 
     constructor(n: string) {
         this.name = n;
@@ -8,12 +9,29 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }   
+
+    printEmployeeInformation() {
+        console.log('employees length: ', this.employees.length);
+        console.log('employees: ', this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
 console.log(accounting);
 accounting.describe();
+accounting.name = "IT"
+accounting.describe()
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };   
-accountingCopy.describe();
+accounting.addEmployee('Mohammad');
+accounting.addEmployee('Max');
+accounting.printEmployeeInformation();
+
+// accounting.employees[2] = 'Ayaz'; // this is a bug, we want to make sure that here we can add employee just by using addEmployee method
+
+// const accountingCopy = { name: "Dummy", describe: accounting.describe }; // has error
+// accountingCopy.describe();
