fbcc2495b02109127b3dad2e6921c786a753ba7a
ayazadeh <m.ayazadeh@gmail.com>
2024-10-20 08:55:35 +0330

    TS: project 4 -> Interfaces as Function Types

--- 4/dist/app.js
+++ 4/dist/app.js
@@ -5,10 +5,15 @@ class Person {
         this.name = n;
     }
     greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+        console.log(phrase + " " + this.name);
     }
 }
 let user1;
-user1 = new Person('Mohammad');
-user1.greet('Hi there - I am');
+user1 = new Person("Mohammad");
+user1.greet("Hi there - I am");
 console.log(user1);
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
+console.log('function interface: ', add(12, 30));
--- 4/src/app.ts
+++ 4/src/app.ts
@@ -3,34 +3,47 @@
 
 // inheritance in interfaces
 interface Named {
-    readonly name: string;
+	readonly name: string;
 }
 
 // we can extend more than one interface by separating them by comma
 // example: interface Greetable extends Named, AnotherInterface
 interface Greetable extends Named {
-    greet(phrase: string): void;
+	greet(phrase: string): void;
 }
 
 // we can implement more than one interface by separating them by comma
 // example: class Person implements Greetable, AnotherInterface
 class Person implements Greetable {
-    name: string;
-    age = 30;
+	name: string;
+	age = 30;
 
-    constructor(n: string) {
-        this.name = n;
-    }
+	constructor(n: string) {
+		this.name = n;
+	}
 
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
-    }
+	greet(phrase: string) {
+		console.log(phrase + " " + this.name);
+	}
 }
 
 let user1: Greetable;
 
-user1 = new Person('Mohammad');
+user1 = new Person("Mohammad");
 // user1.name = 'Max'; // error because name is readonly
 
-user1.greet('Hi there - I am');
+user1.greet("Hi there - I am");
 console.log(user1);
+
+// type addFn = (a: number, b: number) => number;
+// use interface instead of type alias
+interface addFn {
+	(a: number, b: number): number;
+}
+
+let add: addFn;
+
+add = (n1: number, n2: number) => {
+	return n1 + n2;
+};
+console.log('function interface: ', add(12, 30));
