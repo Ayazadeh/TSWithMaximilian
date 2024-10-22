92a5be7da28437310ddd073727a4ed79fe197b77
ayazadeh <m.ayazadeh@gmail.com>
2024-10-22 09:33:13 +0330

    TS: project 5 -> Type Guards

--- 5/dist/app.js
+++ 5/dist/app.js
@@ -7,3 +7,45 @@ const e1 = {
 console.log('intersection types: ', e1);
 const uni = 10;
 console.log('universal type: ', uni);
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('-----------------');
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Max', startDate: new Date() });
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo ...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    console.log('>>>>>>>>');
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- 5/src/app.ts
+++ 5/src/app.ts
@@ -27,3 +27,67 @@ type Universal = Combinable & Numeric; // it'll be number
 
 const uni: Universal = 10;
 console.log('universal type: ', uni);
+
+// End of intersection types
+
+// type guards
+
+// allows us to utilize the flexibility of union types gives us and still ensure that our code runs correctly at runtime
+// we can use 'typeof' or 'in' or 'instanceof' for type guards
+function add(a: Combinable, b: Combinable) {
+	if (typeof a === 'string' || typeof b === 'string') { // type guard
+		return a.toString() + b.toString();
+	}
+	return a + b;
+}
+
+type UnkknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnkknownEmployee) {
+	console.log('-----------------');
+	console.log('Name: ' + emp.name);
+	if ('privileges' in emp) { // type guard
+		console.log('Privileges: ' + emp.privileges);
+	}
+	if ('startDate' in emp) { // type guard
+		console.log('Start Date: ' + emp.startDate);
+	}
+} 
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Max', startDate: new Date() });
+
+class Car {
+	drive() {
+		console.log('Driving...');
+	}
+}
+
+class Truck {
+	drive() {
+		console.log('Driving a truck...');
+	}
+
+	loadCargo(amount: number) {
+		console.log('Loading cargo ...' + amount);
+	}
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+	console.log('>>>>>>>>');
+	
+	vehicle.drive();
+	if (vehicle instanceof Truck) { // type guard
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+// End type guards
