ca421e8847eaa3f5bcc90a4b4f797f4c80154149
ayazadeh <m.ayazadeh@gmail.com>
2024-10-23 08:31:53 +0330

    TS: project 5 -> Discriminated Unions

--- 5/dist/app.js
+++ 5/dist/app.js
@@ -1,47 +1,47 @@
 "use strict";
 const e1 = {
-    name: 'Mohammad',
-    privileges: ['create-server'],
+    name: "Mohammad",
+    privileges: ["create-server"],
     startDate: new Date(),
 };
-console.log('intersection types: ', e1);
+console.log("intersection types: ", e1);
 const uni = 10;
-console.log('universal type: ', uni);
+console.log("universal type: ", uni);
 function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
+    if (typeof a === "string" || typeof b === "string") {
         return a.toString() + b.toString();
     }
     return a + b;
 }
 function printEmployeeInformation(emp) {
-    console.log('-----------------');
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
+    console.log("-----------------");
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
     }
-    if ('startDate' in emp) {
-        console.log('Start Date: ' + emp.startDate);
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
     }
 }
 printEmployeeInformation(e1);
-printEmployeeInformation({ name: 'Max', startDate: new Date() });
+printEmployeeInformation({ name: "Max", startDate: new Date() });
 class Car {
     drive() {
-        console.log('Driving...');
+        console.log("Driving...");
     }
 }
 class Truck {
     drive() {
-        console.log('Driving a truck...');
+        console.log("Driving a truck...");
     }
     loadCargo(amount) {
-        console.log('Loading cargo ...' + amount);
+        console.log("Loading cargo ..." + amount);
     }
 }
 const v1 = new Car();
 const v2 = new Truck();
 function useVehicle(vehicle) {
-    console.log('>>>>>>>>');
+    console.log(">>>>>>>>");
     vehicle.drive();
     if (vehicle instanceof Truck) {
         vehicle.loadCargo(1000);
@@ -49,3 +49,17 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("<<<<<<<<<<<<<");
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
--- 5/src/app.ts
+++ 5/src/app.ts
@@ -14,11 +14,11 @@ type ElevatedEmployee = Admin & Employee; // intersection type
 // its like: interface ElevatedEmployee extends Admin, Employee {}
 
 const e1: ElevatedEmployee = {
-	name: 'Mohammad',
-	privileges: ['create-server'],
+	name: "Mohammad",
+	privileges: ["create-server"],
 	startDate: new Date(),
 };
-console.log('intersection types: ', e1);
+console.log("intersection types: ", e1);
 
 type Combinable = string | number;
 type Numeric = number | boolean;
@@ -26,7 +26,7 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric; // it'll be number
 
 const uni: Universal = 10;
-console.log('universal type: ', uni);
+console.log("universal type: ", uni);
 
 // End of intersection types
 
@@ -35,7 +35,8 @@ console.log('universal type: ', uni);
 // allows us to utilize the flexibility of union types gives us and still ensure that our code runs correctly at runtime
 // we can use 'typeof' or 'in' or 'instanceof' for type guards
 function add(a: Combinable, b: Combinable) {
-	if (typeof a === 'string' || typeof b === 'string') { // type guard
+	if (typeof a === "string" || typeof b === "string") {
+		// type guard
 		return a.toString() + b.toString();
 	}
 	return a + b;
@@ -44,32 +45,34 @@ function add(a: Combinable, b: Combinable) {
 type UnkknownEmployee = Employee | Admin;
 
 function printEmployeeInformation(emp: UnkknownEmployee) {
-	console.log('-----------------');
-	console.log('Name: ' + emp.name);
-	if ('privileges' in emp) { // type guard
-		console.log('Privileges: ' + emp.privileges);
+	console.log("-----------------");
+	console.log("Name: " + emp.name);
+	if ("privileges" in emp) {
+		// type guard
+		console.log("Privileges: " + emp.privileges);
 	}
-	if ('startDate' in emp) { // type guard
-		console.log('Start Date: ' + emp.startDate);
+	if ("startDate" in emp) {
+		// type guard
+		console.log("Start Date: " + emp.startDate);
 	}
-} 
+}
 
 printEmployeeInformation(e1);
-printEmployeeInformation({ name: 'Max', startDate: new Date() });
+printEmployeeInformation({ name: "Max", startDate: new Date() });
 
 class Car {
 	drive() {
-		console.log('Driving...');
+		console.log("Driving...");
 	}
 }
 
 class Truck {
 	drive() {
-		console.log('Driving a truck...');
+		console.log("Driving a truck...");
 	}
 
 	loadCargo(amount: number) {
-		console.log('Loading cargo ...' + amount);
+		console.log("Loading cargo ..." + amount);
 	}
 }
 
@@ -79,10 +82,11 @@ const v1 = new Car();
 const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
-	console.log('>>>>>>>>');
-	
+	console.log(">>>>>>>>");
+
 	vehicle.drive();
-	if (vehicle instanceof Truck) { // type guard
+	if (vehicle instanceof Truck) {
+		// type guard
 		vehicle.loadCargo(1000);
 	}
 }
@@ -91,3 +95,35 @@ useVehicle(v1);
 useVehicle(v2);
 
 // End type guards
+
+// Discriminated unions
+// that makes implementing type guards easier
+
+interface Bird {
+	type: "bird";
+	flyingSpeed: number;
+}
+
+interface Horse {
+	type: "horse";
+	runningSpeed: number;
+}
+
+type animal = Bird | Horse;
+
+function moveAnimal(animal: animal) {
+	// if (animal instanceof Bird) // we can't use instanseof here because js doesn't know interfaces
+	let speed;
+	switch (animal.type) {
+		case "bird":
+			speed = animal.flyingSpeed;
+			break;
+		case "horse":
+			speed = animal.runningSpeed;
+			break;
+	}
+	console.log("<<<<<<<<<<<<<");
+	console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
