5099defc7fe428ac6ee98e6ad5c936cff05de15e
ayazadeh <m.ayazadeh@gmail.com>
2024-10-25 21:36:40 +0330

    TS: project 6 -> Built-in Generics

--- 6/README.md
+++ 6/README.md
@@ -1,1 +1,2 @@
 ### includes
+* Generic Types
---
+++ 6/src/app.ts
@@ -0,0 +1,10 @@
+// Generic types
+
+const names: Array<string> = [] // string[]
+names[0].split(' ')
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done!')
+    }, 2000)
+})
