df021c7d68dcfa1440eb017d59c9bf09c4c13420
ayazadeh <m.ayazadeh@gmail.com>
2024-10-26 09:17:39 +0330

    TS: project 6 -> Creating a Generic Function

--- 6/dist/app.js
+++ 6/dist/app.js
@@ -1,1 +1,19 @@
 "use strict";
+const names = ["test"];
+names[0].split(" ");
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("This is done!");
+    }, 2000);
+});
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+console.log("merge: ", merge({ name: "Mohammad" }, { age: 26 }));
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+function newMerge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const newMergedObj = newMerge({ name: "Max" }, { age: 30 });
+console.log(newMergedObj);
+console.log(newMergedObj.age);
--- 6/src/app.ts
+++ 6/src/app.ts
@@ -1,10 +1,28 @@
 // Generic types
 
-const names: Array<string> = [] // string[]
-names[0].split(' ')
+const names: Array<string> = ["test"]; // string[]
+names[0].split(" "); // if we use <string>, we can use all string methods
 
 const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!')
-    }, 2000)
-})
+	setTimeout(() => {
+		resolve("This is done!");
+	}, 2000);
+});
+
+function merge(objA: object, objB: object) {
+	return Object.assign(objA, objB);
+}
+
+console.log("merge: ", merge({ name: "Mohammad" }, { age: 26 }));
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+// mergedObj.name // error
+
+function newMerge<T extends object, U extends object>(objA: T, objB: U) {
+	return Object.assign(objA, objB);
+}
+
+const newMergedObj = newMerge({ name: "Max" }, { age: 30 });
+
+console.log(newMergedObj);
+console.log(newMergedObj.age);
