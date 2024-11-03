3ba094463e13e3a5958595f326e07c13425732c7
ayazadeh <m.ayazadeh@gmail.com>
2024-11-03 08:57:36 +0330

    TS: project 7 -> Accessor & Parameter Decorators

--- 7/dist/app.js
+++ 7/dist/app.js
@@ -6,6 +6,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
     console.log('LOGGER FACTORY (first decorator)');
     return function (constructor) {
@@ -45,6 +48,27 @@ function Log(target, propertyName) {
     console.log('Property decorator!');
     console.log(target, propertyName);
 }
+function Log2(target, name, descriptor) {
+    console.log('---------------------');
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('---------------------');
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('---------------------');
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 class Product {
     set price(val) {
         if (val > 0) {
@@ -65,3 +89,11 @@ class Product {
 __decorate([
     Log // Property Decorator
 ], Product.prototype, "title", void 0);
+__decorate([
+    Log2 // Accessor Decorator
+], Product.prototype, "price", null);
+__decorate([
+    Log3 // Method Decorator
+    ,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithText", null);
--- 7/src/app.ts
+++ 7/src/app.ts
@@ -44,11 +44,37 @@ function Log(target: any, propertyName: string | Symbol) {
 	console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log('---------------------');
+	console.log('Accessor decorator!');
+	console.log(target);
+	console.log(name);
+	console.log(descriptor) ;
+
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+	console.log('---------------------');
+	console.log('Method decorator!');
+	console.log(target);
+	console.log(name);
+	console.log(descriptor) ;
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log('---------------------');
+	console.log('Parameter decorator!');
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+
 class Product {
 	@Log // Property Decorator
 	title: string;
 	private _price: number;
 
+	@Log2 // Accessor Decorator
 	set price(val: number) {
 		if (val > 0) {
 			this._price = val;
@@ -62,7 +88,8 @@ class Product {
 		this._price = p;
 	}
 
-	getPriceWithText(tax: number) {
+	@Log3 // Method Decorator
+	getPriceWithText(@Log4 tax: number) {
 		return this._price * (1 + tax);
 	}
 }
