1d7e7b36c8923d471cc896616c4be670f053d084
ayazadeh <m.ayazadeh@gmail.com>
2024-11-30 09:18:52 +0330

    TS: project 12 -> Adding Middleware & Types

--- 12_nodejs_express_ts/src/app.ts
+++ 12_nodejs_express_ts/src/app.ts
@@ -1,5 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import todoRouter from "./routes/todos";
 
 const app = express();
+
+app.use("/todos", todoRouter);
+
+// error handling middleware    
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
